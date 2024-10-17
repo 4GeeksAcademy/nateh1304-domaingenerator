297c2ee757252877c8e41bb6bc595bcce8bd82c5
nateh1304 <NATHAN12HABTE@GMAIL.COM>
2024-10-17 15:43:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,34 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird", "Nathan"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-function getRandom(anyArray) {
-  let max = anyArray.length - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
-}
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
   console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML =
-    getRandom(who) +
-    " " +
-    getRandom(action) +
-    " " +
-    getRandom(what) +
-    " " +
-    getRandom(when);
+
+  let domaincomboNames = [];
+  for (let a of pronoun) {
+    for (let b of adj) {
+      for (let c of noun) {
+        domaincomboNames.push(`${a}${b}${c}.com`);
+      }
+    }
+  }
+
+  console.log(domaincomboNames);
+  document.querySelector("#excuse").innerHTML = domaincomboNames.join("<h1>");
 };
